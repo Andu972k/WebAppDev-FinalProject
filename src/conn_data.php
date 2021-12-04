1f5f5ad86a7629a1e1ac46f36492b95552f06ac8
Andu972k <heyanders@gmail.com>
2021-12-04 18:32:31 +0100

    Initial project setup

---
+++ src/conn_data.php
@@ -0,0 +1,16 @@
+<?php
+    //Local DB
+
+    $server = 'localhost';
+    $dbName = 'chinook_abridged';
+    $user = 'root';
+    $pwd = '';
+
+
+    //Azure db
+    //$server = 'localhost';
+    //$dbName = 'chinook_abridged';
+    //$user = 'root';
+    //$pwd = '';
+
+?>
