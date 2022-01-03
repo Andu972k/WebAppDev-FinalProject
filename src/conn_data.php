622f2454cd21551d210ef0c1a1d6d23db286d293
Andu972k <heyanders@gmail.com>
2022-01-03 20:39:18 +0100

    Added composer file

--- src/conn_data.php
+++ src/conn_data.php
@@ -1,12 +1,19 @@
 <?php
     //Local DB
 
-    $server = 'localhost:8889';
+    //$server = 'localhost:8889';
+    //$dbName = 'chinook_abridged';
+    //$user = 'root';
+    //$pwd = '';
+
+
+
+    //Azure DB
+
+    $server = 'music-shop-database.mysql.database.azure.com';
     $dbName = 'chinook_abridged';
-    $user = 'root';
-    $pwd = '';
-
-
+    $user = 'ande972k';
+    $pwd = 'WebAppDev972';
     
 
 ?>
